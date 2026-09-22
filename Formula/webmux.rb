85d373201641c69b220047be7dc42b1cec444628
jkh <jkh@localhost>
2026-09-21 17:18:20 -0700

    build: update Homebrew to v1.3.13 (refs #97)

--- Formula/webmux.rb
+++ Formula/webmux.rb
@@ -1,8 +1,8 @@
 class Webmux < Formula
   desc "Browser-based workspace for persistent terminals and remote desktops"
   homepage "https://github.com/jordanhubbard/webmux"
-  url "https://github.com/jordanhubbard/webmux/archive/refs/tags/v1.3.12.tar.gz"
-  sha256 "ac798313a116ba8584b904702c96ebad5b9a6898ae459724f40856380062c022"
+  url "https://github.com/jordanhubbard/webmux/archive/refs/tags/v1.3.13.tar.gz"
+  sha256 "454ec90f43c61855cf071a70d8917fda76bb9583535fb39c00d41b0012adf0dd"
   license "BSD-2-Clause"
   head "https://github.com/jordanhubbard/webmux.git", branch: "main"
 
